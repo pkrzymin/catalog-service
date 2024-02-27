b312edfe10151429c89b5253ec66dad1a80a6bbd
Piotr Krzyminski <piotr.krzyminski96@gmail.com>
2024-02-27 20:53:36 +0100

    Initial project structure

---
+++ src/test/java/pl/krzyminski/catalogservice/CatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.krzyminski.catalogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
